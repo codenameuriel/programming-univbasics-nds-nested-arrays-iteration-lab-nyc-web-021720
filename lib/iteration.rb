255901f92629a13282b551cf8e55a007bbbeed33
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-19 06:32:53 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -43,10 +43,9 @@ def total_even_pairs(src)
   row_index = 0
   while row_index < src.length do
     element_index = 0
-    evens = []
     while element_index < src[row_index].length do
         if src[row_index][element_index] % 2 == 0
-          evens << src[row_index][element_index]
+          answer << src[row_index][element_index]
         end
         element_index += 1
     end
