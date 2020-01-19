3e137785b22c13fa049eefa0f415adaae0cf8aed
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-19 06:00:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -20,22 +20,22 @@ end
 
 def find_greater_pair(src)
   answer = []
-        row_index = 0 
-        while row_index < src.length do
-            element_index = 0
-            while element_index < src[row_index].length do
-                x = src[row_index][0]
-                if x < src[row_index][element_index]
-                    x = src[row_index][element_index]
-                    answer << x
-                else
-                    answer << x 
-                end
-                element_index += 1
-            end
-            row_index += 1
-        end
-        return answer
+  row_index = 0 
+  while row_index < src.length do
+    element_index = 0
+    while element_index < src[row_index].length do
+       x = src[row_index][0]
+       if x < src[row_index][element_index]
+        x = src[row_index][element_index]
+        answer << x
+       else
+        answer << x 
+       end
+       element_index += 1
+    end
+    row_index += 1
+  end
+  return answer
 end
 
 def total_even_pairs(src)
