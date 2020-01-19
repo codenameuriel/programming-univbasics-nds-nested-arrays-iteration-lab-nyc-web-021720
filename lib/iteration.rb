93fd4028e18f40587a628f73a494626841166d9f
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-01-19 06:45:51 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,59 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  new_array = []
+  row_index = 0
+  while row_index < src.length do 
+    element_index = 0 
+    string = "I love "
+    while element_index < src[row_index].length do
+      if element_index == 0 
+        string += "#{src[row_index][element_index]} and "
+      else
+        string += "#{src[row_index][element_index]} on my pizza"
+      end
+      element_index += 1 
+    end
+    new_array << string
+    row_index += 1
+  end
+  return new_array 
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  answer = []
+  row_index = 0 
+  while row_index < src.length do
+    element_index = 0
+    sub_answer = []
+    while element_index < src[row_index].length do
+      sub_answer << src[row_index][element_index]
+       element_index += 1
+    end
+    if sub_answer[0] > sub_answer[1]
+      answer << sub_answer[0]
+    else
+      answer << sub_answer[1]
+    end
+    row_index += 1
+  end
+  return answer
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  total = 0
+  row_index = 0
+  while row_index < src.length do
+    element_index = 0
+    evens = []
+    while element_index < src[row_index].length do
+        if src[row_index][element_index] % 2 == 0
+          evens << src[row_index][element_index]
+        end
+        element_index += 1
+    end
+    if evens.length == 2 
+      total += evens[0] + evens[1]
+    end
+    row_index += 1 
+  end
+  return total
 end
